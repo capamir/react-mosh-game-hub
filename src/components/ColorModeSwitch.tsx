25974cf854b0da289067d0695d15da290e27c670
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-20 12:30:38 +0330

    Build the color mode button

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { MdWbSunny } from "react-icons/md";
+
+const ColorModeSitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text>
+        {colorMode === "dark" ? <BsFillMoonStarsFill /> : <MdWbSunny />}
+      </Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSitch;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import { logo } from "../assets";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize="60px" />
-      <Text>Navbar</Text>
+    <HStack justifyContent="space-between" paddingX={4}>
+      <HStack>
+        <Image src={logo} boxSize="60px" />
+        <Text>ML Course</Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 };
