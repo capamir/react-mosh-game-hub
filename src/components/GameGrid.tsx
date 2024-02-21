e60523b5f0de5a59bf8fddb47586c271dba6057a
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-21 19:17:05 +0330

    Show loading skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,10 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
-import GameCard from "./GameCard";
+import { GameCard, GameCardSkeleton } from "./index";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, isLoading, error } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   if (error) return <Text>{error}</Text>;
   return (
@@ -12,6 +13,8 @@ const GameGrid = () => {
       padding="10px"
       spacing={10}
     >
+      {isLoading &&
+        skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
       {games.map((game) => (
         <GameCard key={game.id} game={game} />
       ))}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Navbar } from "./Navbar";
 export { default as GameGrid } from "./GameGrid";
+export { default as GameCard } from "./GameCard";
 export { default as PlatformIconList } from "./PlatformIconList";
 export { default as CriticScore } from "./CriticScore";
+export { default as GameCardSkeleton } from "./GameCardSkeleton";
