753995ed53d08f87ab92015131d74d361d1941d9
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-28 00:49:42 +0330

    getting games using react-query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ const GameGrid: React.FC<Props> = ({ gameQuery }) => {
   const { data, isLoading, error } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
@@ -24,7 +24,7 @@ const GameGrid: React.FC<Props> = ({ gameQuery }) => {
               <GameCardSkeleton />
             </GameCardContainer>
           ))
-        : data.map((game) => (
+        : data?.results.map((game) => (
             <GameCardContainer key={game.id}>
               <GameCard game={game} />
             </GameCardContainer>
