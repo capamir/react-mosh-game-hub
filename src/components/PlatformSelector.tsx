82f53272f2870f140270f5ce19135e5ae2ef000b
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-28 00:11:27 +0330

    platform using react query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -2,23 +2,27 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 import { Platform } from "../hooks/useGames";
+import React from "react";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
   selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector: React.FC<Props> = ({
+  onSelectPlatform,
+  selectedPlatform,
+}) => {
   const { data, error } = usePlatforms();
 
-  if (error) return error;
+  if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             key={platform.id}
             onClick={() => onSelectPlatform(platform)}
